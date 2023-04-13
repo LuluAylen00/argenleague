0645fb23551f5d0defb0e3d4d3f7defec31aedf1
Aylen <luluaylen00@gmail.com>
2023-04-13 02:08:54 -0300

    Arreglado bug 4to duelo

--- src/models/index.js
+++ src/models/index.js
@@ -91,7 +91,7 @@ const model = {
                 await db.Partida.update({jugadorUnoId: winner != null ? players.find((p,i) => i != winner) : null},{where: {id: matchId+2}});
                 break;
             case 4:
-                await db.Partida.update({jugadorUnoId: winner != null ? players.find((p,i) => i != winner) : null},{where: {id: matchId+1}});
+                await db.Partida.update({jugadorUnoId: winner != null ? players.find((p,i) => i == winner) : null},{where: {id: matchId+1}});
                 break;
             default:
                 break;
